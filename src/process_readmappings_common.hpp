07d05d7a8702edfbc9402587a014b5bbd5091767
Kevin R. Thornton <krthornt@uci.edu>
2014-11-25 16:20:00 -0800

    moved function

--- src/process_readmappings_common.hpp
+++ src/process_readmappings_common.hpp
@@ -1,11 +1,19 @@
 #ifndef __PECNV_PROECSS_READMAPPINGS_COMMON_HPP__
 #define __PECNV_PROECSS_READMAPPINGS_COMMON_HPP__
 
-#include <string>
 #include <Sequence/bamrecord.hpp>
 #include <Sequence/bamreader.hpp>
+#include <unordered_map>
+#include <string>
+#include <zlib.h>
+
+using readbucket = std::unordered_map<std::string, Sequence::bamrecord>; //name, alignment
 
 std::string toSAM(const Sequence::bamrecord & b,
 		  const Sequence::bamreader & reader);
 
+void updateBucket( readbucket & rb, Sequence::bamrecord & b, 
+		   gzFile csvfile, gzFile samfile,
+		   const char * maptype,
+		   const Sequence::bamreader & reader );
 #endif
